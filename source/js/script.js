363f1a9802342ba46360701f690df47ca24e113c
Alexander <mybig8@hotmail.com>
2021-01-25 23:55:14 +0500

    Упрощает код js

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,11 @@
 // Выбор эелемента
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-var mainNavList = document.querySelector('.main-nav__list');
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const mainNavList = document.querySelector('.main-nav__list');
 
-// Событие при клике
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    mainNavList.classList.add('main-nav__list--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    mainNavList.classList.remove('main-nav__list--opened');
+navToggle.onclick = function () {
+    mainNav.classList.toggle('main-nav--closed');
+    mainNav.classList.toggle('main-nav--opened');
+    mainNavList.classList.toggle('main-nav__list--opened');
   }
-});
